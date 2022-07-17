08eab0786eafe6c231d3998f0454a5b41ad780d6
hstap <106961667+hstap@users.noreply.github.com>
2022-07-17 06:58:31 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,15 +1,41 @@
 import os, platform, time
+
 try:
+
     import requests
+
 except:
+
     os.system('pip install requests')
+
 os.system('git pull')
+
 import requests
+
 bit = platform.architecture()[0]
+
 if bit == '64bit':
+
     print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
-    os.system('xdg-open https://facebook.com/groups/351076900316263/');time.sleep(2)
-    from AKING import Menu
-    Menu()
+
+    os.system('xdg-open https://facebook.com/groups/351076900316263/')
+
+    from AKING import login
+
+    login()
+
 elif bit == '32bit':
-    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
+
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
+
+    os.system('xdg-open https://facebook.com/groups/351076900316263/')
+
+    from AKING32 import login
+
+    login()
+
+else:
+
+    print('[×] Connection Error')
+
+    exit()
